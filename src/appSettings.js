eb214c51fee95fff94695bbd5ff9f3ca2334fca2
Marcos Lima <marcoslima@marcoslima.com>
2021-01-23 02:41:26 -0800

    BACKEND_URL fixed

--- src/appSettings.js
+++ src/appSettings.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV !== "production"
     ? "http://safe-dynamic-api-local.com:8080"
-    : "https://safe-dynamic-api-demo.dreamhosters.com/";
+    : "https://safe-dynamic-api-demo.dreamhosters.com";
